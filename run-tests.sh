83d20e9d9ff139c4dd2f9e7407e698aed5990b01
Andrey Miklashevich <andrey.miklashevichl@wow365.com>
2024-12-24 14:06:35 +0300

    edit

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo "WORKSPACE in run-tests.sh: ${WORKSPACE}"
-docker run \
-    -v ${WORKSPACE}:/tests \
-    golang:1.16.6-alpine3.14 \
-    /tests/scripts/test_in_docker.sh
+
+docker run -v \"${WORKSPACE}:/tests\" golang:1.16.6-alpine3.14 /tests/scripts/test_in_docker.sh
